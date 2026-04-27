99cf26a6143c22ee6c5b940a0e5e027a99e5524a
Mauricio Faria de Oliveira <mfo@igalia.com>
2026-04-27 13:10:57 -0300

    x86/asm, x86/boot: Expose inline memcmp
    Move the inline memcmp function, currently only available to boot code
    (boot/string.c), into the header with similar inline string functions
    (include/asm/string.h) so it may be reused.
    
    Add a _SETUP guard in string.h so not to include the 32/64-bit specific
    string headers in boot/ code (16-bit, real mode) and avoid build errors.
    
    Note: this memcmp() only returns 0/1, not -1/0/1 as regular memcmp(),
    as reported by David Laight. This should be addressed separately.
    
    Signed-off-by: Mauricio Faria de Oliveira <mfo@igalia.com>
    Signed-off-by: Juergen Gross <jgross@suse.com>
    Message-ID: <20260427-pvh-kasan-inline-v2-1-2c57b8dcff6a@igalia.com>

--- arch/x86/include/asm/string.h
+++ arch/x86/include/asm/string.h
@@ -2,10 +2,12 @@
 #ifndef _ASM_X86_STRING_H
 #define _ASM_X86_STRING_H
 
+#ifndef _SETUP
 #ifdef CONFIG_X86_32
 # include <asm/string_32.h>
 #else
 # include <asm/string_64.h>
+#endif
 #endif
 
 static __always_inline void *__inline_memcpy(void *to, const void *from, size_t len)
@@ -27,6 +29,16 @@ static __always_inline void *__inline_memset(void *s, int v, size_t n)
 		     : "a" ((uint8_t)v)
 		     : "memory");
 	return ret;
+}
+
+/* Note: this memcmp() only returns 0/1, not -1/0/1 as regular memcmp(). */
+static __always_inline int __inline_memcmp(const void *s1, const void *s2, size_t len)
+{
+	bool diff;
+
+	asm("repe cmpsb"
+	    : "=@ccnz" (diff), "+D" (s1), "+S" (s2), "+c" (len));
+	return diff;
 }
 
 #endif /* _ASM_X86_STRING_H */
